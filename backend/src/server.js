fe963d62ec45af614da811326461393d853990e4
Yash1386 <yashpratap291@gmail.com>
2026-01-20 09:08:28 +0530

    update server.js to serve frontend in production feat: add root package.json for streamlined build and start scripts

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import express from "express"  
 import dotenv, { configDotenv } from "dotenv"
-
+import path from "path"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 dotenv.config();
@@ -8,8 +8,20 @@ const app=express()
 
 const PORT=process.env.PORT || 3000
 
+const __dirname=path.resolve()
+
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
 app.listen(PORT,()=>{
-    console.log("The server is listening at p000")
+    console.log("The server is listening at port 3000")
 })
+
+if(process.env.NODE_ENV=="Production")
+{
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    //The * symbol loads frontedn whenever anything except the given routes is loaded
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+    })
+}
