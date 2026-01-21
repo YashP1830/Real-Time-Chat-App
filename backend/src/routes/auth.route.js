f89fe23566fafa9ebfa8cf3bdf66c78c03acf252
Yash1386 <yashpratap291@gmail.com>
2026-01-21 19:04:20 +0530

    feat: add Arcjet protection middleware and integrate Arcjet library for enhanced security

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,11 @@
  import express from 'express';
  import { signup , login , logout, updateprofile} from '../controllers/auth.controller.js';
  import { protectRoute } from '../middleware/auth.middleware.js';
+ import {arcjetProtection} from '../middleware/arcjet.middleware.js'
  const router=express.Router()
 
+ router.use(arcjetProtection)
  router.post('/signup',signup)
-
  router.post('/login',login);
  router.post('/logout',logout);
  router.post('/upadate-profile',protectRoute,updateprofile)
